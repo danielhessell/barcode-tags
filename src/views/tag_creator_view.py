6c7cb46712d088a3f2aadd5df981ad543930c09d
Daniel Hessel <danieldaniabreu@gmail.com>
2024-02-06 12:53:52 -0300

    feat: implement tag creator controller

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,5 +1,6 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import TagCreatorController
 
 class TagCreatorView:
     '''
@@ -7,7 +8,10 @@ class TagCreatorView:
     '''
 
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
-        # body = http_request.body
-        # product_code = body["product_code"]
-        print(http_request)
-        return HttpResponse(status_code=200, body={"Hello": "World"})
+        body = http_request.body
+        product_code = body["product_code"]
+
+        tag_creator_controller = TagCreatorController()
+        formatted_response = tag_creator_controller.create(product_code)
+
+        return HttpResponse(status_code=200, body=formatted_response)
