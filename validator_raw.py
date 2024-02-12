1644c2c84c7007da1cf31565f456fa6e5c648010
Daniel Hessel <danieldaniabreu@gmail.com>
2024-02-12 10:12:35 -0300

    chore: test cerberus

---
+++ validator_raw.py
@@ -0,0 +1,36 @@
+from cerberus import Validator
+
+body = {
+  "data": {
+    "element1": 123,
+    "element": "Hello World",
+    "element3": ""
+  }
+}
+
+body_validator = Validator({
+  "data": {
+    "type": "dict",
+    "schema": {
+      "element1": {
+        "type": "float",
+        "required": True,
+        "empty": False
+      },
+      "element2": {
+        "type": "string",
+        "required": True,
+        "empty": True
+      },
+      "element3": {
+        "type": "string",
+        "required": False,
+        "empty": False
+      }
+    }
+  }
+})
+
+response = body_validator.validate(body)
+print(response)
+print(body_validator.errors)
